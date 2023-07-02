823be6030224c48037496f3f648678767e1ecc82
jilvesko <jukka.ilveskorpi@edu.sasky.fi>
2023-07-02 18:42:46 +0300

    to render

--- Osa2/phonebook/backend/index.js
+++ Osa2/phonebook/backend/index.js
@@ -1,4 +1,21 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
     {
@@ -43,11 +60,64 @@ let persons = [
     }
   ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(JSON.stringify(persons))
+app.get('/', (req, res) => {
+  res.send('<h1>Boozzzze book</h1>')
+}) 
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(p => p.id))
+  : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+ 
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server runnin on port ${PORT}`)
+})
--- Osa2/phonebook/frontend/src/services/persons.js
+++ Osa2/phonebook/frontend/src/services/persons.js
@@ -1,32 +1,3 @@
-/* import axios from 'axios';
-
-const baseUrl = 'http://localhost:3001';
-
-const getAll = () => {
-  const request = axios.get(`${baseUrl}/api/persons`);
-  return request.then((response) => response.data);
-}
-
-const create = (personObject) => {
-  const request = axios.post(`${baseUrl}/api/persons`, personObject);
-  return request.then((response) => response.data);
-}
-
-const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/api/persons/${id}`);
-  return request.then((response) => response.data);
-};
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/api/persons/${id}`, newObject);
-  return request.then((response) => response.data);
-}
-
-export default { getAll, create, deletePerson, update }; */
-
-/*
-
-
 import axios from 'axios';
 const baseUrl = 'http://localhost:3001/api/persons';
 
@@ -53,4 +24,3 @@ const update = (id, newObject) => {
 export default { getAll, create, deletePerson, update }; 
 
 
-*/
