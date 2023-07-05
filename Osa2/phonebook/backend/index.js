116354ff8d915544e4f3c27301807fc6ef0128b0
jilvesko <jukka.ilveskorpi@edu.sasky.fi>
2023-07-05 14:48:33 +0300

    phonebook 3.8 step 8

--- Osa2/phonebook/backend/index.js
+++ Osa2/phonebook/backend/index.js
@@ -1,5 +1,144 @@
 const express = require('express')
 const app = express()
+//const cors = require('cors')
+const morgan =  require('morgan');
+const PORT = 3001
+
+//app.use(cors())
+
+let persons = [
+    {
+      id: 1,
+      name: "Bill Gates ",
+      number: "0405522134 "
+    },
+    {
+      id: 2,
+      name: "Dan Abramov ",
+      number: "0507723411 "
+    },
+    {
+      id: 3,
+      name: "Steven Jobs ",
+      number: "05065245611 "
+    },
+    {
+      id: 5,
+      name: "Linda Liukas ",
+      number: "0408772341 "
+    },
+    {
+      id: 6,
+      name: "Jorma Ollila ",
+      number: "040726311 "
+    },
+    {
+      id: 7,
+      name: "Linus Torvalds ",
+      number: "040772341 "
+    },
+    {
+      id: 8,
+      name: "Ilkka Hulkko ",
+      number: "0407723411 "
+    },
+    {
+      id: 9,
+      name: "Ada Locelace ",
+      number: "0412231223 "
+    }
+  ]
+
+  const getRand = () => Math.floor(Math.random() * 10000)
+
+  app.use(express.json());
+  app.use(
+    morgan(function (tokens, request, response) {
+         return [
+          tokens.method(request, response),
+          tokens.url(request, response),
+          tokens.status(request, response),
+          tokens.res(request, response, 'content-length'), '-',
+          tokens['response-time'](request, response), 'ms',
+          tokens.method(request) === "POST" ? JSON.stringify(request.body) : "",
+         ].join(''); 
+        })
+  )
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+app.get('/info', (req, res) => {
+  const requestTime = new Date(Date.now()) 
+  const formattedTime = requestTime.toLocaleString();
+  res.write("<p>Phonebook has info for " + persons.length + " people in </p>")
+  res.write("<p>Current time: " + formattedTime + "</p>");
+  res.send()
+}) 
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+// response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end()
+});
+//app.use(unknownEndpoint)
+
+/*const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(p => p.id))
+  : 0
+  return maxId + 1
+} */
+
+app.post('/api/persons', (request, response) => {
+  //const body = request.body
+  console.log(request.body)
+  const content = request.body
+  if (!content) {
+    return response.status(400).json({
+      error: 'no content'
+    })
+  }
+ 
+  if (!content.name || !content.number) {
+    return response.status(400).json({
+      error: "content missing values"
+    })
+  }
+
+  if (persons.find(p => p.name === content.name)) {
+    return response.status(400).json({
+        error: "name must be unique"
+    })
+}
+
+  const newPerson = {
+    ...request.body, id: getRand()
+  }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server runnin on port ${PORT}`)
+})
+
+
+
+/* const express = require('express')
+const app = express()
 const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
@@ -120,4 +259,4 @@ app.use(unknownEndpoint)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server runnin on port ${PORT}`)
-})
+}) */
