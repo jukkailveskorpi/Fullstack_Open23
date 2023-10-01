1df9c5ddc0adbbb2448fc0818fb66b8e7065e9eb
Jeeves <Solarsystems3@gmail.com>
2023-10-01 14:34:32 +0300

    zz

--- Osa2/notes/notes_be/index.js
+++ Osa2/notes/notes_be/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -16,8 +16,10 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 let notes = [
   {
