c22a76bf41a83753995243adae2500652564d4eb
Jeeves <Solarsystems3@gmail.com>
2023-10-02 12:33:16 +0300

    mongoose test

--- Osa2/notes/notes_be/index.js
+++ Osa2/notes/notes_be/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -10,7 +13,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-//zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -22,7 +24,7 @@ app.use(requestLogger)
 app.use(express.static('dist'))
 
 let notes = [
-  {
+  /*{
     id: 1,
     content: "HTML is easy",
     important: true
@@ -36,46 +38,87 @@ let notes = [
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
-  }
+  } */
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+/*const mongoose = require('mongoose')
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
+
+// ÄLÄ KOSKAAN TALLETA SALASANOJA GitHubiin!
+const url =
+  `mongodb+srv://solarsystem3:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)*/
+
+/*app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})*/
+
+/*note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})*/
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
   response.json(notes)
 })
+})
 
-const generateId = () => {
+
+/*const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
   return maxId + 1
-}
+}*/
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+   // date: new Date(),
+   // id: generateId(),
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+
+  /*notes = notes.concat(note)
+  response.json(note)*/
+})
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
+
+ /* const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
 
   if (note) {
@@ -83,8 +126,8 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-  response.json(note)
-})
+  response.json(note)*/
+
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -95,7 +138,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
+// || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
