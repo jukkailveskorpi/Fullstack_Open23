10941045143b162024b4459a6cd4432c59220514
Jeeves <Solarsystems3@gmail.com>
2023-10-15 14:50:20 +0300

    phonebook  morgan test

--- Osa2/notes/notes_be/models/note.js
+++ Osa2/notes/notes_be/models/note.js
@@ -1,18 +1,31 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+//console.log('connecting to', url)
 
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message);
+  });
+
+/*mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+  .catch((error) => {
     console.log('error connecting to MongoDB: ', error.message)
-  })
+  })*/
+
+  //console.log('connecting to', url);
+  //console.log('PORT', PORT);
+
 
 const noteSchema = new mongoose.Schema({
   content: {
