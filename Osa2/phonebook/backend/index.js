591172dd1d7a46b1bd84b08b6f8222741c20878c
Jeeves <Solarsystems3@gmail.com>
2023-10-23 21:55:48 +0300

    zzz

--- Osa2/phonebook/backend/index.js
+++ Osa2/phonebook/backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-app.use(express.static('dist'))
+const morgan = require('morgan')
+require('dotenv').config()
+
 const Person = require('./models/person')
-app.use(cors())
 
 morgan.token('req-body', (req) => {
   if (req.method === 'POST') {
@@ -23,10 +23,12 @@ app.use(
   response.status(404).send({ error: 'unknown endpoint'})
 }*/
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
-    let persons = [
-      {
+     let persons = [
+    /* {
         id: 1,
         name: "Bill Gates ",
         number: "0405522134 "
@@ -80,7 +82,7 @@ app.use(express.json())
         name: "Peter Forsell",
         number: "0408822345",
         id: 13
-      }
+      }*/
     ]
 
 app.get('/api/persons', (request, response) => {
@@ -89,44 +91,38 @@ app.get('/api/persons', (request, response) => {
 })
 })
 
-const generatedId = () => {
+/*const generatedId = () => {
   const maxId = persons.length > 0
   ? Math.max(...persons.map(p => p.id))
   : 0
   return maxId + 1
-}
+}*/
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-  return response.status(400).json({
-    error: 'name missing'
-  }) 
+  if (body.name === undefined) {
+  return response.status(400).json({ error: 'name missing' }) 
 }
 
-const person = {
+const person = new Person({
   name: body.name,
   number: body.number,
-  id: generatedId(),
-}
+})
+  //id: generatedId(),
 
-persons = persons.concat(person)
-
-response.json(person)
+person.save().then(savedPerson => {
+response.json(savedPerson)
+})
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id) 
-  const person = persons.find(person => person.id === id)
+  Person.findById(request.params.id)
+  .then(person => {
   //console.log(person.id, typeof person.id, id, typeof id, person.id === id)
-
-  if (person) {
+  
   response.json(person)
-  } else {
-    response.status(404).end()
-  }
-  response.json
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -137,7 +133,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 //app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
--- Osa2/phonebook/backend/models/person.js
+++ Osa2/phonebook/backend/models/person.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -6,9 +7,8 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 
-console.log('connecting to', url)
+//console.log('connecting to', url)
 mongoose.connect(url)
-
   .then(result => {
     console.log('connected to MongoDB')
   })
